a671042b210c6bbe1da893c23603f19f394dc790
kana-sucre <guillaume-pillon@live.fr>
2014-06-07 18:14:27 +0200

    c

--- src/fr/utc/payutc/MainActivity.java
+++ src/fr/utc/payutc/MainActivity.java
@@ -2,14 +2,33 @@ package fr.utc.payutc;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.tab);
+		
+		TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+		
+		TabSpec firstTabSpec = tabHost.newTabSpec("tid1");
+		TabSpec secondTabSpec = tabHost.newTabSpec("tid1");
+
+		/** TabSpec setIndicator() is used to set name for the tab. */
+		/** TabSpec setContent() is used to set content for a particular tab. */
+		firstTabSpec.setIndicator("First Tab Name").setContent(new Intent(this, FirstTab.class));
+		secondTabSpec.setIndicator("Second Tab Name").setContent(new Intent(this, SecondTab.class));
+
+		/** Add tabSpec to the TabHost to display. */
+		tabHost.addTab(firstTabSpec);
+		tabHost.addTab(secondTabSpec);
+		
 	}
 
 	@Override
