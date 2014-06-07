27896c66c1a4d564961727a8f254de2ef766e39b
Beurier <p.beurier@gmail.com>
2014-06-07 18:10:03 +0200

    gvy

---
+++ src/fr/utc/payutc/RechargementTab.java
@@ -0,0 +1,16 @@
+package fr.utc.payutc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RechargementTab extends Activity {
+
+   /** Called when the activity is first created. */
+      
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_rechargement_tab);
+       }
+}
